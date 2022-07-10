897832f874acdc152232233bab717731b9ce4088
HenestrosaConH <henestrosaconh@gmail.com>
2022-07-10 17:12:26 +0200

    Added No numerals (missing C++)

---
+++ Code coach/Medium/No numerals/no_numerals.py
@@ -0,0 +1,25 @@
+import re
+
+# In a real use-case, we would use the num2word library
+numsToWord = {
+	"0": "zero",
+	"1": "one",
+	"2": "two",
+	"3": "three",
+	"4": "four",
+	"5": "five",
+	"6": "six",
+	"7": "seven",
+	"8": "eight",
+	"9": "nine",
+	"10": "ten",
+}
+
+text = input()
+pattern = re.compile(r'\b\d+\b')
+
+for m in re.finditer(pattern, text):
+	if m.group(0) in numsToWord.keys():
+		text = text.replace(m.group(0), numsToWord[m.group(0)], 1)
+
+print(text)
