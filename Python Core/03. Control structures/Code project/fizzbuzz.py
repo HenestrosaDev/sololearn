937ea77a64da2c91343df704a5a204ece1762be4
HenestrosaConH <henestrosaconh@gmail.com>
2023-02-22 13:03:03 +0100

    Modify FizzBuzz code to make it more efficient

--- Python Core/03. Control structures/Code project/fizzbuzz.py
+++ Python Core/03. Control structures/Code project/fizzbuzz.py
@@ -1,13 +1,19 @@
-n = int(input())
+def odd_fizzbuzz(num: int):
+    for x in range(1, num, 2):
+        output = ""
 
-for x in range(1, n):
-	if x % 2 == 0:
-		continue
-	if x % 3 == 0 and x % 5 == 0:
-		print("SoloLearn")
-	elif x % 3 == 0:
-		print("Solo")
-	elif x % 5 == 0:
-		print("Learn")
-	else:
-		print(x)
+        if x % 3 == 0:
+            output += "Solo"
+
+        if x % 5 == 0:
+            output += "Learn"
+
+        if not output:
+            print(x)
+        else:
+            print(output)
+
+
+if __name__ == "__main__":
+    n = int(input())
+    odd_fizzbuzz(n)
