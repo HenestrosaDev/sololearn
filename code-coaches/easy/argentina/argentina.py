e56986b3162df5b92e88ddf0abb3758ea3a91773
HenestrosaConH <henestrosaconh@gmail.com>
2023-05-07 13:49:19 +0200

    Refactor code

--- code-coaches/easy/argentina/argentina.py
+++ code-coaches/easy/argentina/argentina.py
@@ -1,12 +1,10 @@
 from decimal import Decimal
 
-ars = Decimal(input()) # Argentine peso
+ars = Decimal(input())  # Argentine peso
 usd = Decimal(input())  # US dollar
 
-ars_to_usd = Decimal('0.02')  # exchange rate
+ars_to_usd = Decimal("0.02")  # exchange rate
 converted_usd = ars * ars_to_usd
 
-if converted_usd > usd:
-	print("Dollars")
-else:
-	print("Pesos")
+result = "Dollars" if converted_usd > usd else "Pesos"
+print(result)
