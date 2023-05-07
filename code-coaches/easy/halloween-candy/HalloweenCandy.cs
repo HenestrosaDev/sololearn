4a63aab79af7a6c49769e0ffd65d76e48c05f27c
HenestrosaConH <henestrosaconh@gmail.com>
2023-05-07 14:52:19 +0200

    Add C# code

---
+++ code-coaches/easy/halloween-candy/HalloweenCandy.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class HalloweenCandy
+{
+    public static void Main(string[] args)
+    {
+        int houses;
+
+        do
+        {
+            houses = Convert.ToInt32(Console.ReadLine());
+        }
+        while (houses < 3);
+
+        int billProbability = (int) Math.Ceiling(100.0 / houses * 2);
+        Console.WriteLine(billProbability);
+    }
+}
