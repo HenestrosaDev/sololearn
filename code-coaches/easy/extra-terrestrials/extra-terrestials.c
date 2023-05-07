094ae0c42149da05ce67a9ef198041717346ef15
HenestrosaConH <henestrosaconh@gmail.com>
2023-05-07 13:55:35 +0200

    Add C code

---
+++ code-coaches/easy/extra-terrestrials/extra-terrestials.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char* str)
+{
+    int len = strlen(str);
+    for (int i = 0; i < len / 2; i++)
+    {
+        char temp = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = temp;
+    }
+}
+
+int main()
+{
+    char text[100];
+    scanf("%s", text);
+    reverse(text);
+    printf("%s\n", text);
+    return 0;
+}
