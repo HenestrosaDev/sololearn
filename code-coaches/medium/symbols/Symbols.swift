9a560c2fbf1dbf511bc80bd4e8c03081d0392dc3
HenestrosaConH <henestrosaconh@gmail.com>
2023-05-09 18:30:39 +0200

    Add Swift code

---
+++ code-coaches/medium/symbols/Symbols.swift
@@ -0,0 +1,17 @@
+func removeSymbols(from encoded: String) -> String {
+    // symbols: a char that is not alphanumeric or a space
+    return encoded.filter {
+        let isLetter = ($0 >= "a" && $0 <= "z") || ($0 >= "A" && $0 <= "Z")
+        let isNumber = $0 >= "0" && $0 <= "9"
+        let isWhitespace = $0 == " "
+        return isLetter || isNumber || isWhitespace
+    }
+}
+
+func main() {
+    if let encoded = readLine() {
+        print(removeSymbols(from: encoded))
+    }
+}
+
+main()
