1e30ce2b2d97ae1143f7835df434d5eac9225aee
HenestrosaConH <henestrosaconh@gmail.com>
2023-05-09 11:40:09 +0200

    Add C# code

---
+++ code-coaches/medium/no-numerals/NoNumerals.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+
+class AverageWorldLength
+{
+    static string ReplaceNumbersWithWords(string sentence)
+    {
+        string[] numberWords = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+
+        string[] words = sentence.Split();
+        string[] updatedWords = words.Select(word =>
+            int.TryParse(word, out int number) && number <= 10 ? numberWords[number] : word
+        ).ToArray();
+
+        return string.Join(" ", updatedWords);
+    }
+
+    static void Main(string[] args)
+    {
+        string sentence = Console.ReadLine();
+        Console.WriteLine(ReplaceNumbersWithWords(sentence));
+    }
+}
