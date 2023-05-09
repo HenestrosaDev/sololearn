4c708f66759b84f3097f7302d075ab1cb4dab628
HenestrosaConH <henestrosaconh@gmail.com>
2023-05-09 19:32:47 +0200

    Add C# code

---
+++ code-coaches/medium/the-spy-life/TheSpyLife.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using System.Text;
+
+class TheSpyLife
+{
+    static string DecodeMessage(string message)
+    {
+        StringBuilder decodedMessage = new StringBuilder();
+
+		foreach (char c in message.ToCharArray())
+		{
+			if (char.IsLetter(c) || char.IsWhiteSpace(c))
+				decodedMessage.Append(c);
+		}
+
+        string flippedDecodedMessage = new string(decodedMessage.ToString().Reverse().ToArray());
+		return flippedDecodedMessage;
+    }
+
+	static void Main()
+	{
+		string encodedMessage = Console.ReadLine();
+		Console.WriteLine(DecodeMessage(encodedMessage));
+	}
+}
