5a4d9e97de5d96074490d5507a3d227378358f9b
HenestrosaConH <henestrosaconh@gmail.com>
2023-05-09 15:55:14 +0200

    Add Swift code

---
+++ code-coaches/medium/secret-message/SecretMessage.swift
@@ -0,0 +1,26 @@
+func encodeMessage(_ message: String) -> String {
+    let asciiLowercaseA = 97
+    let asciiLowercaseZ = 122
+    var encodedMessage = ""
+
+    for character in message {
+        if ((character >= "a" && character <= "z") || (character >= "A" && character <= "Z")) {
+            let asciiValue = Int(String(character).lowercased().unicodeScalars.first!.value)
+            let invertedAsciiValue = asciiLowercaseZ - (asciiValue - asciiLowercaseA)
+            let invertedCharacter = Character(UnicodeScalar(invertedAsciiValue)!)
+            encodedMessage.append(invertedCharacter)
+        } else {
+            encodedMessage.append(character)
+        }
+    }
+
+    return encodedMessage
+}
+
+func main() {
+    if let message = readLine() {
+        print(encodeMessage(message))
+    }
+}
+
+main()
