1accc1743e994eb74fdd0d74d2b81a06e47ffeae
HenestrosaConH <henestrosaconh@gmail.com>
2023-05-09 20:40:37 +0200

    Add C# code

---
+++ code-coaches/medium/youtube-link-finder/YouTubeLinkFinder.cs
@@ -0,0 +1,32 @@
+using System;
+
+class YouTubeLinkFinder
+{
+    static string ExtractVideoId(string url)
+    {
+        if (url.Contains("youtube.com/watch"))
+        {
+            string[] parts = url.Split(new string[] { "v=" }, StringSplitOptions.None);
+            if (parts.Length == 2)
+            {
+                return parts[1];
+            }
+        }
+        else if (url.Contains("youtu.be/"))
+        {
+            string[] parts = url.Split('/');
+            if (parts.Length > 0)
+            {
+                return parts[parts.Length - 1];
+            }
+        }
+        throw new ArgumentException("Invalid YouTube video URL");
+    }
+
+    static void Main()
+    {
+        string url = Console.ReadLine();
+        string videoId = ExtractVideoId(url);
+        Console.WriteLine(videoId);
+    }
+}
