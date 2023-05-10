c0c1a91f40c9473f6b98e553ec06600f03d9e554
HenestrosaConH <henestrosaconh@gmail.com>
2023-05-10 11:57:42 +0200

    Add C# code

---
+++ code-coaches/hard/new-drivers-license/NewDriversLicense.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class NewDriversLicense
+{
+    static int GetWaitingTime(string myName, int numAgents, List<string> customers)
+    {
+        int timeForLicense = 20;
+        int positionInQueue = customers.IndexOf(myName) + 1;
+        int waitingTime = (int) Math.Ceiling((double) positionInQueue / numAgents) * timeForLicense;
+        return waitingTime;
+    }
+
+    static void Main(string[] args)
+    {
+        string myName = Console.ReadLine();
+        int numAgents = int.Parse(Console.ReadLine());
+        List<string> customers = Console.ReadLine().Split().ToList();
+
+        customers.Add(myName);
+        customers.Sort();
+        Console.WriteLine(GetWaitingTime(myName, numAgents, customers));
+    }
+}
