d214006a1a961d01ad42298c35f50650a5bab5e0
HenestrosaConH <henestrosaconh@gmail.com>
2023-05-12 15:28:49 +0200

    Remove namespace from C# file

--- code-coaches/easy/duct-tape/DuctTape.cs
+++ code-coaches/easy/duct-tape/DuctTape.cs
@@ -1,41 +1,38 @@
 using System;
 
-namespace DuctTape
+class DuctTape
 {
-    class Program
+    static int GetArea(int height, int width)
     {
-        static int GetArea(int height, int width)
-        {
-            return height * width;
-        }
+        return height * width;
+    }
 
-        static int ToInches(int feet)
-        {
-            return feet * 12;
-        }
+    static int ToInches(int feet)
+    {
+        return feet * 12;
+    }
 
-        static int GetDuctTapesNeeded(int height, int width)
-        {
-            // Everything is calculated in inches
-            int ductTapeHeight = 720;
-            int ductTapeWidth = 2;
+    static int GetDuctTapesNeeded(int height, int width)
+    {
+        // Everything is calculated in inches
+        int ductTapeHeight = 720;
+        int ductTapeWidth = 2;
 
-            int doorAreaBothSides = GetArea(height, width) * 2;
-            int ductTapeArea = GetArea(ductTapeHeight, ductTapeWidth);
+        int doorAreaBothSides = GetArea(height, width) * 2;
+        int ductTapeArea = GetArea(ductTapeHeight, ductTapeWidth);
 
-            int ductTapesNeeded = (int)Math.Ceiling((double)doorAreaBothSides / ductTapeArea);
-            return ductTapesNeeded;
-        }
+        int ductTapesNeeded = (int)Math.Ceiling((double)doorAreaBothSides / ductTapeArea);
+        return ductTapesNeeded;
+    }
 
-        static void Main(string[] args)
-        {
-            int doorHeightFeet = int.Parse(Console.ReadLine());
-            int doorWidthFeet = int.Parse(Console.ReadLine());
+    static void Main(string[] args)
+    {
+        int doorHeightFeet = int.Parse(Console.ReadLine());
+        int doorWidthFeet = int.Parse(Console.ReadLine());
 
-            int doorHeightInches = ToInches(doorHeightFeet);
-            int doorWidthInches = ToInches(doorWidthFeet);
+        int doorHeightInches = ToInches(doorHeightFeet);
+        int doorWidthInches = ToInches(doorWidthFeet);
 
-            Console.WriteLine(GetDuctTapesNeeded(doorHeightInches, doorWidthInches));
-        }
+        Console.WriteLine(GetDuctTapesNeeded(doorHeightInches, doorWidthInches));
     }
 }
